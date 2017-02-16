d4070da26e8de7d6142fc900cb68adf8aef6019c
Garrett Sitter <gmsitter@gmail.com>
2017-02-15 21:58:34 -0500

    app.js changes
    Currently, API call only pulls most recent tweet.  Committing this to have a record of functioning code while I experiment with changing the call.
    
    Want to ideally randomize on the server before making the API call in order to minimize the amount of data returned.

--- app.js
+++ app.js
@@ -9,9 +9,33 @@ var client = new Twitter({
   bearer_token: keyinfo.TWITTER_BEARER_TOKEN
 });
 
-var params = {screen_name: 'dril'};
+/*
+  create a new random date starting Mon Sep 15 17:21:54 +0000 2008
+  1221499314000
+*/
+var year = Math.floor(Math.random() * 9) + 2009;
+var month = Math.floor(Math.random() * 12) + 1;
+var day = Math.floor(Math.random() * 31) + 1;
+
+dateString = year + "-" + month + "-" + day
+console.log(dateString);
+
+var params = {
+  screen_name: 'dril',
+  count: 1, //only pull 1 tweet max.  Minimize API traffic by randomizing first.
+  since: dateString,
+  until: dateString
+};
+
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
+    console.log(response);
+    console.log("\n\n");
     console.log(tweets);
+  } else {
+    console.log("error\n");
+    console.log(error);
   }
 });
+
+var dateString = "";
