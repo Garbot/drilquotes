6227c16573f0b111b75cce5d07041eec0d17805e
Garrett Sitter <gmsitter@gmail.com>
2017-02-17 14:12:47 -0500

    added server to .js file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var Twitter = require('twitter');
+var http = require('http');
 
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -11,19 +12,31 @@ var client = new Twitter({
 //establish parameters for our request
 var params = {
   screen_name: 'dril',
-  count: 200
+  count: 5
 };
 
-//call the user timeline API using our parameters
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    //console.log(tweets);
-    var random = Math.floor(Math.random() * params.count) + 1;
-    var selectedTweet = tweets[random].id;
-    return selectedTweet;
-  } else {
-    console.log("error:\n");
-    console.log(error);
-    return error;
-  }
+
+//function to call the user timeline API using our parameters
+function getTweet(){
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      //console.log(tweets);
+      var random = Math.floor(Math.random() * params.count) + 1;
+      var selectedTweet = tweets[random].id;
+      console.log(selectedTweet);
+      return selectedTweet;
+    } else {
+      console.log("error:\n");
+      console.log(error);
+      return error;
+    }
+  });
+}
+
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end(getTweet);
 });
+
+server.listen(8080);
