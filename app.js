ee0fdd2e7851995916bdc0e89a2901989c43d62e
Garrett Sitter <gmsitter@gmail.com>
2017-02-18 14:27:48 -0500

    added code to allow cross origin requests

--- app.js
+++ app.js
@@ -37,13 +37,13 @@ function getTweet(callback){
 //create web server.  Upon valid request, call the getTweet function.
 var server = http.createServer(function(request, response) {
     response.setHeader('Content-Type', 'application/javascript');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Request-Method', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    if ( req.method === 'OPTIONS' ) {
-      res.writeHead(200);
-      res.end();
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Request-Method', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    if ( request.method === 'OPTIONS' ) {
+      response.writeHead(200);
+      response.end();
       return;
     }
     //pass an anonymous function as callback to getTweet.  in this callback, we write
